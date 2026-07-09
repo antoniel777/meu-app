338fd8fa6ad715c9631e1393c4d9bce727b52ddb
antoniel777 <antoniel_oliveira@estudante.sesisenai.org.br>
2026-07-09 16:45:37 -0300

    dd

--- components/evento-lista.tsx
+++ components/evento-lista.tsx
@@ -1,54 +1,33 @@
-import React, { useState } from 'react';
-import { FlatList, RefreshControl, StyleSheet } from 'react-native'; // Importamos o StyleSheet aqui
+import { consultarEventos } from '@/services/api-service';
+import { useEffect, useState } from 'react';
+import { Alert, FlatList } from 'react-native';
 import EventoItem from './evento-item';
 
-const eventosIniciais = [
-  {
-    id: '1',
-    titulo: 'Pesca da Tainha',
-    imagem: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS4Kb1A5tRjk-nb2JUueOdX77XG5HiS4AkvqlTs0Pj0w1WQ6sA84NnGVS4&s=10', 
-    descricao: 'A pesca da tainha será intensa no Pântano do Sul',
-    local: 'Pântano do Sul, Florianópolis/SC',
-    data: '30/Maio',
-    valor: 99.99
-  },
-  {
-    id: '2',
-    titulo: 'Arrial do Senai',
-    imagem: 'https://www.uema.br/wp-content/uploads/2017/07/arraial.jpg',
-    descricao: 'Muita festa, quentão e pipoca',
-    local: 'Distrito Industrial, São José/SC',
-    data: '25/Junho',
-    valor: 25.50
-  },
-  {
-    id: '3',
-    titulo: 'Arrial do Sesi',
-    imagem: 'https://t3.ftcdn.net/jpg/20/39/30/20/240_F_2039302027_aLCbtXZ4xyoYmFlMGLys2I4FZMuQAK2I.jpg', 
-    descricao: 'Muitos prêmios e diversão',
-    local: 'Pagani, Palhoça/SC',
-    data: '12/Junho',
-    valor: 49.90
-  }
-];
-
 export default function EventoLista() {
-  const [atualizando, setAtualizando] = useState(false);
-  const [listaEventos, setListaEventos] = useState(eventosIniciais);
+  const [eventos, setEventos] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
 
-  const aoAtualizar = () => {
-    setAtualizando(true);
-    setTimeout(() => {
-      setListaEventos([...eventosIniciais]);
-      setAtualizando(false);
-    }, 2000);
-  };
+  async function atualizarTela() {
+    try {
+      setIsLoading(true);
+
+      const response = await consultarEventos();
+
+      setEventos(response);
+    } catch (err) {
+      Alert.alert(err.message);
+    } finally {
+      setIsLoading(false);
+    }
+  }
+
+  useEffect(() => {
+    atualizarTela();
+  }, []);
 
   return (
     <FlatList
-
-
-      data={listaEventos}
+      data={eventos}
       renderItem={({ item }) => (
         <EventoItem
           titulo={item.titulo}
@@ -60,22 +39,8 @@ export default function EventoLista() {
         />
       )}
       keyExtractor={(item) => item.id}
-      refreshControl={
-        <RefreshControl 
-          refreshing={atualizando} 
-          onRefresh={aoAtualizar} 
-          colors={['#689F38']} 
-          tintColor="#689F38" 
-        />
-      }
+      refreshing={isLoading}
+      onRefresh={atualizarTela}
     />
   );
-}
-
-
-const styles = StyleSheet.create({
-
-  container: {
-    flex: 1,
-  }
-});
+} 
